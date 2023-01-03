dce52fb11272eba00bab5a0b81ac6f3ac199a8a0
Anthony Dell'Anno <anthony@dellanno.co.uk>
2023-01-03 16:22:04 +0000

    added api tests

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import decodeRoute from './src/routes/decode/decode'
 import encodeRoute from './src/routes/encode/encode'
@@ -6,19 +6,15 @@ import helmet from 'helmet';
 
 dotenv.config();
 
-const app: Express = express();
+export const app: Express = express();
 const port = process.env.PORT;
 
 app.use(helmet())
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`Server is running on port ${port}`);
-});
-
 app.use('/decode', decodeRoute)
 app.use('/encode', encodeRoute)
 
-app.listen(port, () => {
+export const server = app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- src/routes/encode/encode.ts
+++ src/routes/encode/encode.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express';
+import express, { Router } from 'express';
 import { encodeController } from '../../controllers/encode/encode'
 
 const router: Router = express.Router()
